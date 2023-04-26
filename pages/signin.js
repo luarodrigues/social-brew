2c6634124591a3ac9cf9e9ee5fa96220d74fc298
Luana Rodrigues <luaa.rodrigues@gmail.com>
2023-04-26 15:42:08 +0100

    apply brand colours to sign in page

--- pages/signin.js
+++ pages/signin.js
@@ -15,17 +15,25 @@ import {
 
 export default function SimpleCard() {
   return (
-    <Flex
-      minH={"100vh"}
-      align={"center"}
-      justify={"center"}
-      bg={useColorModeValue("gray.50", "gray.800")}
-    >
-      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+    <Flex minH={"100vh"} align={"center"} justify={"center"} bg={"#f7bd96"}>
+      <Stack
+        spacing={8}
+        mx={"auto"}
+        maxW={"lg"}
+        py={12}
+        px={6}
+        fontFamily={"avenir"}
+      >
         <Stack align={"center"}>
-          <Heading fontSize={"4xl"}>Sign in page</Heading>
-          <Text fontSize={"lg"} color={"gray.600"}>
-            to enjoy all of our cool <Link color={"blue.400"}>features</Link> ✌️
+          <Text fontSize={"66px"} color={"white"} textTransform={"uppercase"}>
+            Social brew
+          </Text>
+          <Heading fontSize={"4xl"} color={"white"} textTransform={"uppercase"}>
+            Sign in page
+          </Heading>
+          <Text fontSize={"lg"} color={"white"}>
+            to enjoy all of our cool <Link color={"gray.700"}>features</Link>{" "}
+            ☕️
           </Text>
         </Stack>
         <Box
@@ -50,13 +58,15 @@ export default function SimpleCard() {
                 justify={"space-between"}
               >
                 <Checkbox>Remember me</Checkbox>
-                <Link color={"blue.400"}>Forgot password?</Link>
+                <Link color={"#f7bd96"}>Forgot password?</Link>
               </Stack>
               <Button
-                bg={"blue.400"}
+                bg={"#f7bd96"}
                 color={"white"}
                 _hover={{
-                  bg: "blue.500",
+                  bg: "white",
+                  color: "#f7bd96",
+                  border: "2px solid #f7bd96",
                 }}
               >
                 Sign in
