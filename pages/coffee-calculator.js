eaf9df41a821d4b3d7ecfa67fbcb6acdd22dbe91
Luana Rodrigues <luaa.rodrigues@gmail.com>
2023-05-02 14:38:49 +0100

    Added more brew methods and improved the code

--- pages/coffee-calculator.js
+++ pages/coffee-calculator.js
@@ -21,24 +21,34 @@ const CoffeeCalculator = () => {
   const handleCalculate = () => {
     let ratio;
     switch (brewMethod) {
-      case "French Press":
-        ratio = 17;
+      case "Espresso":
+        ratio = 2;
         break;
-      case "V60":
-        ratio = 15;
+      case "V60 or Kalita":
+        ratio = 16;
         break;
       case "AeroPress":
         ratio = 17;
         break;
-      case "Espresso":
-        ratio = 2;
+      case "French Press":
+        ratio = 18;
         break;
-      default:
-        ratio = 16;
+      case "Cold Brew (concentrate)":
+        ratio = 5;
+        break;
+      case "Cold Brew":
+        ratio = 10;
         break;
     }
-    const calculatedWaterAmount = coffeeAmount * ratio;
-    setWaterAmount(calculatedWaterAmount);
+
+    {
+      if (coffeeAmount === "" || isNaN(coffeeAmount) || brewMethod === "") {
+        setWaterAmount("Please enter a valid amount of coffee");
+      } else {
+        const calculatedWaterAmount = coffeeAmount * ratio;
+        setWaterAmount(calculatedWaterAmount);
+      }
+    }
   };
 
   return (
@@ -46,20 +56,32 @@ const CoffeeCalculator = () => {
       minH={"100vh"}
       align={"center"}
       justify={"center"}
-      bg={"#f7bd96"}
+      bg={"#0F606B "}
       fontFamily={"avenir"}
     >
       <title>coffee calculator</title>
-      <Stack spacing={10} mx={"auto"} maxW={"lg"} py={10} px={6}>
+      <Stack
+        spacing={5}
+        mx={"auto"}
+        maxW={"lg"}
+        py={10}
+        px={6}
+        bg={"#A7D2DD"}
+        borderRadius={"40px"}
+      >
         <Stack align={"center"}>
           <Text fontSize={"66px"} color={"white"} textTransform={"uppercase"}>
             Let's brew ☕️
           </Text>
-          <Heading fontSize={"sm"} color={"white"} textTransform={"uppercase"}>
-            Social brew's Coffee Calculator
+          <Heading
+            fontSize={"large"}
+            color={"white"}
+            textTransform={"uppercase"}
+          >
+            Coffee Calculator
           </Heading>
         </Stack>
-        <Box color={"gray.700"}>
+        <Box color={"#0F606B"}>
           <FormControl id="coffee-amount" mb={4}>
             <FormLabel>Coffee Amount (in grams)</FormLabel>
             <Input
@@ -75,19 +97,30 @@ const CoffeeCalculator = () => {
               value={brewMethod}
               onChange={(e) => setBrewMethod(e.target.value)}
             >
-              <option value="French Press">French Press</option>
-              <option value="V60">V60</option>
-              <option value="AeroPress">AeroPress</option>
               <option value="Espresso">Espresso</option>
+              <option value="V60 or Kalita">V60 or Kalita</option>
+              <option value="AeroPress">AeroPress</option>
+              <option value="French Press">French Press</option>
+              <option value="Cold Brew (concentrate)">
+                Cold Brew (concentrate)
+              </option>
+              <option value="Cold Brew">Cold Brew</option>
             </Select>
           </FormControl>
-          <Button mb={4} onClick={handleCalculate} color={"#f7bd96"}>
-            Calculate Water Amount
-          </Button>
+          <Box align={"center"} justify={"center"}>
+            <Button
+              borderRadius="20"
+              mb={4}
+              onClick={handleCalculate}
+              color={"#FD6853"}
+            >
+              Calculate Water Amount
+            </Button>
+          </Box>
           {waterAmount && (
             <Stack>
               <Box>
-                Water Amount: <strong>{waterAmount} g</strong>
+                Water Amount(in grams): <strong>{waterAmount}</strong>
               </Box>
             </Stack>
           )}
