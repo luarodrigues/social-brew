bfef3b9f64f3e276e111a69b0d68d83099806957
Luana Rodrigues <luaa.rodrigues@gmail.com>
2023-05-24 18:04:37 +0100

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import { ChakraProvider } from "@chakra-ui/react";
-
 import React from "react";
 
 export default function App({ Component, pageProps }) {
@@ -9,3 +8,13 @@ export default function App({ Component, pageProps }) {
     </ChakraProvider>
   );
 }
+
+///CREATE CHACKRA THEME
+// import theme from "./theme";
+// export default function App({ Component, pageProps }) {
+//   return (
+//     <ChakraProvider theme={theme}>
+//       <Component {...pageProps} />
+//     </ChakraProvider>
+//   );
+// }
