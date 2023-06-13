df1ccb61cc7db9bea3a115146bc9f4a7ece9066c
Luana Rodrigues <luaa.rodrigues@gmail.com>
2023-06-13 17:03:01 +0100

    Bug fixed

--- pages/sign-in-page.js
+++ pages/sign-in-page.js
@@ -45,15 +45,6 @@ export default function SignInCard() {
   return (
     <Flex minH={"100vh"} align={"center"} justify={"center"} bg={"#393E46"}>
       <Head>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-39S3TF2VHV"
-        ></script>
-        <script>
-          window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'G-39S3TF2VHV');
-        </script>
         <title>sign in page</title>
       </Head>
 
